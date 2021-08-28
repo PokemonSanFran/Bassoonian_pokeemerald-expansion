b65471d5df6eb6130cc3e5c144ebc8188e5798dc
Bassoonian <iasperbassoonian@gmail.com>
2021-08-29 00:50:43 +0200

    More boops

--- src/new_game.c
+++ src/new_game.c
@@ -126,7 +126,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(INSIDE_OF_TRUCK), MAP_NUM(INSIDE_OF_TRUCK), -1, -1, -1);
+    SetWarpDestination(MAP_GROUP(LITTLEROOT_TOWN_BRENDANS_HOUSE_1F), MAP_NUM(LITTLEROOT_TOWN_BRENDANS_HOUSE_1F), 2, 5, 6);
     WarpIntoMap();
 }
 
