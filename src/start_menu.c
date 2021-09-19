5dad3b0478d2e139441c8612cd0ff7f232d28176
Bassoonian <iasperbassoonian@gmail.com>
2021-09-19 17:12:59 +0200

    Remove redundant save confirmation

--- src/start_menu.c
+++ src/start_menu.c
@@ -1021,7 +1021,8 @@ static u8 SaveFileExistsCallback(void)
     }
     else
     {
-        ShowSaveMessage(gText_AlreadySavedFile, SaveConfirmOverwriteCallback);
+        //ShowSaveMessage(gText_AlreadySavedFile, SaveConfirmOverwriteCallback);
+		sSaveDialogCallback = SaveSavingMessageCallback;
     }
 
     return SAVE_IN_PROGRESS;
